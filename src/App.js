0031df0fac6860a8b284d1d490d96d13be3de82c
erdemkeren <erdemkeren@gozen.systems>
2019-04-20 18:09:45 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const styles = {
+  screen: {
+    width: "300px",
+    height: "120px",
+    margin: "50px auto",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "rgb(249, 171, 27)",
+    padding: "20px",
+    userSelect: "none"
+  },
+  numberBox: {
+    width: "70px",
+    height: "100px",
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 3,
+    backgroundColor: "#FFF"
+  },
+  numberContainer: {
+    fontFamily: "'Pattaya', sans-serif",
+    fontSize: 48,
+    textAlign: "center",
+    display: "block"
+  }
+};
+
+const PullButton = props => <div onClick={props.onClick}>Pull</div>;
+
+const NumberBox = props => {
+  return (
+    <div style={styles.numberBox}>
+      <span style={styles.numberContainer}>{props.number}</span>
+    </div>
+  );
+};
+
+const Won = props => <div>You won! Jackpot!</div>;
+
+class Screen extends React.Component {
+  state = {
+    won: false,
+    numbers: []
+  };
+
+  generateNumber = () => Math.floor(Math.random() * 10);
+
+  generateNumbers = () => {
+    return [
+      this.generateNumber(),
+      this.generateNumber(),
+      this.generateNumber()
+    ];
+  };
+
+  pull = () => {
+    const numbers = this.generateNumbers();
+    const won = numbers.reduce((acc, number) => {
+      return acc && number === 7;
+    }, []);
+
+    this.setState({ won, numbers });
+  };
+
+  componentDidMount() {
+    this.pull();
+  }
+
+  render() {
+    const { won, numbers } = this.state;
+
+    return (
+      <div style={styles.screen}>
+        {won && <Won />}
+        {!won &&
+          numbers.map((number, i) => <NumberBox key={i} number={number} />)}
+        {!won && <PullButton onClick={this.pull} />}
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Screen />
       </div>
     );
   }
